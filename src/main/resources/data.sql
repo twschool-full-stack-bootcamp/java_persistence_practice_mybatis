0ce47af5f495b24beb98bb0fb561f0f74ee24cb9
zjx-immersion <jianxin_home@163.com>
2019-08-23 23:02:50 +0800

    [Jason] schema and data sql in resource

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO employee (id, name, age) VALUES (1, 'jianxin', 20);
+INSERT INTO employee (id, name, age) VALUES (2, 'Ajie', 30);
+INSERT INTO employee (id, name, age) VALUES (3, 'Joy', 28);
+INSERT INTO employee (id, name, age) VALUES (4, 'Marry', 22);
+INSERT INTO employee (id, name, age) VALUES (5, 'AJ', 29);
+INSERT INTO employee (id, name, age) VALUES (6, 'Baibai', 40);
+INSERT INTO employee (id, name, age) VALUES (7, 'Amanda', 350);
